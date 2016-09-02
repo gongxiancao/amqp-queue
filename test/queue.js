e78e0aa7304f2bcb31433c7ff0bbd156270eb72a
GongXian Cao <gongxiancao@hotmail.com>
2016-09-02 15:38:26 +0800

    use mongo store view information to easy query

--- test/queue.js
+++ test/queue.js
@@ -1,36 +1,72 @@
 'use strict';
 
-var Queue = require('../lib/queue');
-console.log('starting ...');
-var should = require('chai').should();
+var Queue = require('../lib/queue'),
+  async = require('async'),
+  should = require('chai').should(),
+  sinon = require('sinon');
+
 /* globals
   describe: false,
   it: false
   */
 
 describe('queue', function() {
-  it('should job get saved', function (done) {
+  it('should job get processed', function (done) {
     var queue = new Queue('test', {
       amqp: {host: 'localhost'},
-      redis: {
-        host: 'localhost',
-        port: 6379,
-        prefix: 'test'
+      view: {
+        redis: {
+          host: 'localhost',
+          port: 6379,
+        },
+        mongo: {
+          hosts: [{ host: 'localhost', port: 27017}],
+          database: 'test'
+        }
       }
     });
 
     queue.process('testJob', function (job, done) {
-      setTimeout(function () {
-      job.progress(0.1, 'step1...');
-      job.progress(0.5, 'step2...');
-      done();
-      }, 100);
-      // done(null, {});
+      should.equal(job.state, 'active');
+      console.log('in processing...');
+      async.series([
+        function (done) {
+          setTimeout(done, 200);
+        },
+        function (done) {
+          job.progress(0.2, 'step 1...', done);
+        },
+        function (done) {
+          setTimeout(done, 200);
+        },
+        function (done) {
+          job.progress(0.4, 'step 2...', done);
+        },
+        function (done) {
+          setTimeout(done, 200);
+        },
+        function (done) {
+          job.progress(0.6, 'step 3...', done);
+        },
+        function (done) {
+          setTimeout(done, 200);
+        },
+        function (done) {
+          job.progress(0.9, 'step 4...', done);
+        }
+      ], done);
     });
+
+    var progressSpy = sinon.spy();
+
     this.timeout(5000);
     var job = queue.create('testJob', {});
     job.on('complete', function (data) {
       should.exist(data.id);
+      should.equal(data.state, 'complete');
+      should.exist(data.completedAt);
+      should.exist(data.duration);
+      should.equal(progressSpy.callCount, 4);
       done();
     });
 
@@ -38,9 +74,7 @@ describe('queue', function() {
       should.not.exist(err);
       should.exist(job.id);
       queue.get(job.id, function (err, job) {
-        job.on('progress', function () {
-          console.log('progress...', arguments);
-        });
+        job.on('progress', progressSpy);
       });
     });
   });
